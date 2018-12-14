d353b568b03bb09fce2b1c7137eb66acfc10bc9f
Ezequiel Flores <supereze.bxb@gamil.com>
2018-12-14 11:24:54 -0600

    antes de crudrepository

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, created_at) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, created_at) VALUES(2, 'Jhon', 'Doe', 'jhon.doe@email.com', '2017-08-28');
