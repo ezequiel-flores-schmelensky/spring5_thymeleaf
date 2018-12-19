34902eaf405552bc1fd26511be4378e800867688
Ezequiel Flores <supereze.bxb@gamil.com>
2018-12-18 18:21:14 -0600

    reinicializando carpeta uploads

--- src/main/java/com/bolsadeideas/springboot/app/models/service/IUploadFileService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IUploadFileService.java
@@ -11,5 +11,7 @@ public interface IUploadFileService {
 	public Resource load(String filename) throws MalformedURLException;
 	public String copy(MultipartFile file) throws IOException;
 	public boolean delete(String filename);
+	public void deleteAll();
+	public void init() throws IOException;
 	
 }
