d08aaf3c150e710db854f88d352a192b24223300
Ezequiel Flores <supereze.bxb@gamil.com>
2018-12-26 12:13:12 -0600

    agregando links login y logout

--- src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
@@ -12,9 +12,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login(@RequestParam(value="error", required=false) String error, Model model, Principal principal, RedirectAttributes flash) {
+	public String login(@RequestParam(value="error", required=false) String error, @RequestParam(value="logout", required=false) String logout, Model model, Principal principal, RedirectAttributes flash) {
 		
-		if(principal != null) {
+		if (principal != null) {
 			flash.addFlashAttribute("info","Ya ha iniciado sesión anteriormente");
 			return "redirect:/";
 		}
@@ -23,6 +23,9 @@ public class LoginController {
 			model.addAttribute("error", "Error en el login: nombre de usuario o contraseña incorrecta, por favor volver a intentarlo");
 		}
 		
+		if (logout != null) {
+			model.addAttribute("success", "Ha cerrado sesión con éxito");
+		}
 		return "login";
 	}
 }
