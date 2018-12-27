0a1056f561e2015808b5a1dbd1b136316536cc7f
Ezequiel Flores <supereze.bxb@gamil.com>
2018-12-27 16:24:46 -0600

    agregando lenguajes primeros paso configurando beans e interceptor

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,10 +1,16 @@
 package com.bolsadeideas.springboot.app;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer{
@@ -30,5 +36,26 @@ public class MvcConfig implements WebMvcConfigurer{
 	@Bean //registrar un bean en el contenedor de spring
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+		return localeResolver;
+	}
+	
+	//Interseptor para cambiar el lenguage cada que se envie
+	//despues se tiene que registrar con source->override->addInterceptors
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 	}
 }
