fdd30c29698a79bd6a244a161467a0ef11bf09e5
Ezequiel Flores <supereze.bxb@gamil.com>
2018-12-26 18:36:21 -0600

    otra forma de asignar seguridad a las rutas con secured

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -17,6 +17,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -53,6 +55,7 @@ public class ClienteController {
 	@Autowired
 	private IUploadFileService uploadFileService;
 	
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
 	@GetMapping(value="/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
 		
@@ -69,6 +72,7 @@ public class ClienteController {
 				.body(recurso);
 	}
 	
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping(value="/ver/{id}")
 	public String ver(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente = clienteService.fetchByIdWithFacturas(id);//clienteService.findById(id);
@@ -124,6 +128,7 @@ public class ClienteController {
 		return "listar";
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value="/form")
 	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
@@ -132,6 +137,7 @@ public class ClienteController {
 		return "form";
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@RequestMapping(value="/form/{id}")
 	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente = null;
@@ -150,6 +156,7 @@ public class ClienteController {
 		return "form";
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value="/form", method=RequestMethod.POST)
 	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
 		if(result.hasErrors()) {
@@ -184,6 +191,7 @@ public class ClienteController {
 		return "redirect:/listar";
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value="/eliminar/{id}")
 	public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash) {
 		
