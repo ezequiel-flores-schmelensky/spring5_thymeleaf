5a73b59643a7d37b930518fe3ca9b1af46240bd0
Ezequiel Flores <supereze.bxb@gamil.com>
2018-12-28 09:25:42 -0600

    agregando mas detalles al pdf

--- src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/pdf/FacturaPdfView.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.bolsadeideas.springboot.app.models.entity.Factura;
+import com.bolsadeideas.springboot.app.models.entity.ItemFactura;
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 
@@ -28,7 +31,7 @@ public class FacturaPdfView extends AbstractPdfView {
 		tabla.addCell(factura.getCliente().getEmail());
 		
 		PdfPTable tabla2 = new PdfPTable(1);
-		tabla.setSpacingAfter(20);
+		tabla2.setSpacingAfter(20);
 		tabla2.addCell("Datos de la Factura");
 		tabla2.addCell("Folio: " + factura.getId());
 		tabla2.addCell("Descripción: " + factura.getDescription());
@@ -36,6 +39,28 @@ public class FacturaPdfView extends AbstractPdfView {
 		
 		document.add(tabla);
 		document.add(tabla2);
+		
+		PdfPTable tabla3 = new PdfPTable(4);
+		tabla3.setSpacingAfter(20);
+		tabla3.addCell("Producto");
+		tabla3.addCell("Precio");
+		tabla3.addCell("Cantidad");
+		tabla3.addCell("Total");
+		
+		for (ItemFactura item: factura.getItems()) {
+			tabla3.addCell(item.getProducto().getNombre());
+			tabla3.addCell(item.getProducto().getPrecio().toString());
+			tabla3.addCell(item.getCantidad().toString());
+			tabla3.addCell(item.calcularImporte().toString());
+		}
+		
+		PdfPCell cell = new PdfPCell(new Phrase("Total: "));
+		cell.setColspan(3);
+		cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+		tabla3.addCell(cell);
+		tabla3.addCell(factura.getTotal().toString());
+		
+		document.add(tabla3);
 	}
 	
 }
